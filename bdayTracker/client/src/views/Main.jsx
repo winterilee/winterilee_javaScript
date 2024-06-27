dd571bb0b3e8bd25ef75ca6d91f69fbca5587ec0
winterilee <plumtree9612@gmail.com>
2024-06-26 17:44:16 -0700

    making a progress oin the client folder

---
+++ bdayTracker/client/src/views/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ComponentName = (props) => {
+    return(
+        <div>
+            <h1>Testing</h1>
+        </div>
+    );
+}
+
+export default ComponentName;
