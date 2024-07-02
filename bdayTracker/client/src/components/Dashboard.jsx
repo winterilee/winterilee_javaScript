7f4a5596cac9d9cf2e9c62e380f8517fcf6a1db4
winterilee <plumtree9612@gmail.com>
2024-07-02 16:51:39 -0700

    making a progress in the client folder

--- bdayTracker/client/src/components/Dashboard.jsx
+++ bdayTracker/client/src/components/Dashboard.jsx
@@ -1,9 +1,85 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+// import {Link} from "react-router-dom";
+import axios from 'axios';
 
 const Dashboard = (props) => {
+    const [allBdays, setAllBdays] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/bdays")
+            .than(res => setAllBdays(res.data))
+            .catch(err => console.log(err))
+    }, []);
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [bdayMonth, setBdayMonth] = useState("");
+    const [bdayDay, setBdayDay] = useState("");
+    const isGifted = false;
+    const giftIdeas = "";
+
+    const newBdayHandler = e => {
+        e.preventDefault();
+        const newBday = {
+            firstName: firstName,
+            lastName: lastName,
+            bdayMonth: bdayMonth,
+            bdayDay: bdayDay,
+            isGifted: isGifted,
+            giftIdeas: giftIdeas
+        }
+        axios.post("http://localhost:8000/api/bdays", newBday)
+            .then(res => {setAllBdays([...allBdays, res.data])})
+            .catch(err => console.log(err))
+    };
+
     return(
         <div>
-            <h1>Testing</h1>
+            <h2>Birthday Tracker</h2>
+            <form onSubmit={newBdayHandler}>
+                <div>
+                    <label>First Name</label>
+                    <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)}/>
+                </div>
+                <div>
+                    <label>Last Name</label>
+                    <input type="text" value={lastName} onChange={e => setLastName(e.target.value)}/>
+                </div>
+                <div>
+                    <label>Birth Month</label>
+                    <input type="text" value={bdayMonth} onChange={e => setBdayMonth(e.target.value)}/>
+                </div>
+                <div>
+                    <label>Birth Day</label>
+                    <input type="text" value={bdayDay} onChange={e => setBdayDay(e.target.value)}/>
+                </div>
+                <buttom style={{margin:"10px"}}>+Add a new Birthday</buttom>
+            </form>
+            <h3>Birthday List</h3>
+            <table>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Birthday</th>
+                    <th>Gifted?</th>
+                    <th>Gift Ideas</th>
+                    <th>Action</th>
+                </tr>
+                {
+                    allBdays.map( bday => {
+                        return(
+                            <tr key={bday._id}>
+                                <td>{bday.firstName}</td>
+                                <td>{bday.lastName}</td>
+                                <td>{bday.bdayMonth}/{bday.bdayDay}</td>
+                                <td>{bday.isGifted}</td>
+                                <td>{bday.giftIdeas}</td>
+                                <td><button>Edit</button></td>
+                            </tr>
+                        )
+                    })
+                }
+            </table>
         </div>
     );
 }
